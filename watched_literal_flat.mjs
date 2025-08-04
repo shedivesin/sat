c7a8e73d1a683788ffeac11dd1fa48f2775e39e4
shedivesin <>
2025-08-03 20:47:38 -0600

    full algo, doesn't work

--- watched_literal_flat.mjs
+++ watched_literal_flat.mjs
@@ -10,7 +10,7 @@ function solve(formula) {
     if(!Array.isArray(clause)) { throw new TypeError("Invalid clause"); }
 
     const l = clause.length;
-    if(l < 1) { return []; } // UNSAT
+    if(l < 1) { return null; } // UNSAT
 
     p += l;
 
@@ -56,35 +56,82 @@ function solve(formula) {
     watch[j] = i;
   }
 
-  console.log(
-    "literals=(%s)\nstart=(%s)\nwatch=(%s)\nnext=(%s)",
-    literals.join(" "),
-    start.join(" "),
-    watch.join(" "),
-    next.join(" "),
-  );
-
   // BACKTRACKING SEARCH
   // B1. Initialize.
-  let d = 0;
-
   // B2. Rejoice or choose.
-  // while(d < m) {
+  b2: for(let d = 0; d < n; ) {
     move[d] = (watch[d << 1] >= m) | (watch[(d << 1) | 1] < m);
     let l = (d << 1) | move[d];
+    console.log(
+      "literals=(%s)\nstart=(%s)\nwatch=(%s)\nnext=(%s)\nmove=(%s)",
+      literals.join(" "),
+      start.join(" "),
+      watch.join(" "),
+      next.join(" "),
+      move.join(" "),
+    );
+
     console.log("d=%d move[d]=%d l=%d", d, move[d], l);
 
     // B3. Remove -l if possible.
-    // FIXME
-  //}
+    b3: for(let j = watch[l ^ 1]; j < m; ) {
+      const i = start[j];
+      const i_p = start[j + 1];
+      const j_p = next[j];
+      for(let k = i + 1; k < i_p; k++) {
+        const l_p = literals[k];
+        // If l_p isn't false (e.g. is TBD or is true), then watch it, instead.
+        // FIXME: see (57)
+        if((l_p >> 1) > d || ((l_p + move[l_p >> 1]) & 1) === 0) {
+          literals[i] = l_p;
+          literals[k] = l ^ 1;
+          next[j] = watch[l_p];
+          watch[l_p] = j;
+          j = j_p;
+          continue b3;
+        }
+      }
+
+      // Can't stop watching -l.
+      watch[l ^ 1] = j;
+
+      // B5. Try again.
+      b5: for(;;) {
+        if(move[d] < 2) {
+          move[d] ^= 3;
+          l ^= 1;
+          j = watch[l ^ 1]; // Necessary since we can't GOTO B3 in JavaScript.
+          continue b3;
+        }
+
+        // B6. Backtrack.
+        if(d < 1) { return null; } // UNSAT
+        d--;
+      }
+    }
+
+    // B4. Advance.
+    watch[l ^ 1] = m;
+    d++;
+  }
+
+  // CONVERT OUTPUT AND RETURN
+  const solution = new Array(n);
+  for(let i = 0; i < n; i++) {
+    solution[i] = (i + 1) * (1 - ((move[i] & 1) << 1));
+  }
+
+  return solution;
 }
 
-solve([ 
-  [ 1,  2, -3],
-  [ 2,  3, -4], 
-  [ 3,  4,  1],
-  [ 4, -1,  2],
-  [-1, -2,  3],  
-  [-2, -3,  4],
-  [-3, -4, -1],
-]);
+console.log(
+  solve([ 
+    [ 1,  2, -3],
+    [ 2,  3, -4],
+    [ 3,  4,  1],
+    [ 4, -1,  2],
+    [-1, -2,  3],
+    [-2, -3,  4],
+    [-3, -4, -1],
+  ]),
+);
